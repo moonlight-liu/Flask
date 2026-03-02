5d7e8a0aff31898f27e3f2757298d4415a63292a
adore <1342330846@qq.com>
2026-03-02 16:56:09 +0800

    Add the index page template

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ from markupsafe import escape
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello"
+# @app.route("/")
+# def hello():
+#     return "Hello"
 
 
 @app.route("/user/<name>")
@@ -26,3 +26,28 @@ def test_url_for():
     Test page
     """
     return output
+
+
+# 虚拟数据 用于渲染主页模版
+name1 = "Grey Li"
+movies1 = [
+    {"title": "My Neighbor Totoro", "year": "1988"},
+    {"title": "Dead Poets Society", "year": "1989"},
+    {"title": "A Perfect World", "year": "1993"},
+    {"title": "Leon", "year": "1994"},
+    {"title": "Mahjong", "year": "1996"},
+    {"title": "Swallowtail Butterfly", "year": "1996"},
+    {"title": "King of Comedy", "year": "1999"},
+    {"title": "Devils on the Doorstep", "year": "1999"},
+    {"title": "WALL-E", "year": "2008"},
+    {"title": "The Pork of Music", "year": "2012"},
+]
+
+from flask import Flask, render_template
+
+# ...
+
+
+@app.route("/")
+def index():
+    return render_template("index.html", name=name1, movies=movies1)
